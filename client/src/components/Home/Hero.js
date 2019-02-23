08bfac4fb00d18b7e09de2036009a174b0b57f83
Tasha <tamitchell14@gmail.com>
2019-02-23 15:59:40 -0500

    added img to header

--- client/src/components/Home/Hero.js
+++ client/src/components/Home/Hero.js
@@ -7,6 +7,7 @@ const Hero = () =>
       <h1>Save the Date!</h1>
       <h3>Aunt Dana's 55th Birthday Party</h3>
       </header>
+        <img src={require("../../img/hero-img.jpg")} alt="having a good time"></img>
   </Container>
 
 export default Hero
