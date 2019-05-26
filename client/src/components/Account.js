c96fdf1e1f451410ef73ad0a83701f42b25c6ff7
Tasha Mitchell <tamitchell14@gmail.com>
2019-05-26 09:28:09 -0400

    changed backend structure isGoing to boolean

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -13,19 +13,17 @@ class Account extends React.Component {
     super()
     this.state = {
       isGoing: "",
-      fullName: "",
-      guests: 0
+      fullName: ""
     }
   }
   getUserInfo = () => {
+  
   let userObj = firebase.auth().onAuthStateChanged(user => {
         if (user) {
           firebase
             .database()
             .ref(`users/${user.uid}`)
-            .once("value", snap => {
-              this.setState({ ...snap.val() })
-            })
+            .once("value", snap => this.setState({ ...snap.val() }));
         }
       });
       return userObj
@@ -36,7 +34,8 @@ class Account extends React.Component {
   }
 
   render(){
-    const {fullName, isGoing, guests} = this.state
+    const {fullName, isGoing} = this.state
+    console.log(isGoing)
     return(
 
     <AuthUserContext.Consumer>
@@ -45,13 +44,15 @@ class Account extends React.Component {
     <Container className="acct-form-container">
       <h2>Account</h2>
       <div>
-        <p>{fullName}, here is your current status regarding the birthday party:</p>
-        <p className={isGoing.isGoingGroup === "true" ? "GOING":"NOT-GOING"}>{isGoing.isGoingGroup === "true" ? "GOING":"NOT GOING"}</p>
-        <p>with {guests} {guests === 1 ? "guest" : "guests"} coming.</p>
+        <p>{fullName}, here is your current status regarding the 2019 New Orleans Trip:</p>
+        <p className={isGoing === true ? "GOING":"NOT-GOING"}>{isGoing === true ? "GOING":"NOT GOING"}</p>
         <p>If you would like to change your decision, visit this <Link to={routes.RSVP}>link</Link>.</p>
       </div>
       <div>
-        <p>If you would like to delete your account or reset your password, please contact the <a href="mailto:tamitchell14@gmail.com">system administrator</a></p>
+        <p>You can make account changes with the following links below:
+          <br/>
+          <Link to={routes.PASSWORD_FORGET}>Password Forget</Link> | <Link to={routes.PASSWORD_UPDATE}>Password Reset</Link> | <Link to={routes.DELETE_ACCT}>Delete Your Account</Link>
+        </p>
       </div>
     </Container>
   
