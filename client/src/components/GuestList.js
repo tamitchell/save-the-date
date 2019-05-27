cb1df353c6b31a3aaad5122f10a346557e67f216
Tasha Mitchell <tamitchell14@gmail.com>
2019-05-26 21:38:23 -0400

    added coloring to tables

--- client/src/components/GuestList.js
+++ client/src/components/GuestList.js
@@ -3,12 +3,10 @@ import { FaTimes } from "react-icons/fa";
 import { Table } from "reactstrap";
 import CustomizedModal from "../components/CustomizedModal";
 import { DeleteUser } from "./DeleteUser";
-import { WithLoading } from "./WithLoadingHOC";
 
 
 
 const GuestList = (props) =>  {
-  console.log(props)    
   let guestList = props.guests.map((user, i) => {
     const deleteMsg = `Are you sure you want to remove ${user.fullName} from the list? This action cannot be undone.`
     const confirmDelete = <CustomizedModal buttonLabel={<FaTimes />} modalBody={props.notification} modalFooterAction={<DeleteUser userName={user.fullName} notification={props.notification} deleteMsg={deleteMsg} setNotification={props.setNotification} userID={user.uid}/>}/>
@@ -17,8 +15,8 @@ const GuestList = (props) =>  {
         <td>{user.fullName}</td>
         <td>{user.email}</td>
         <td>{user.phoneNumber}</td>
-        {user.isGoing === null || user.isGoing === undefined ? "Not Determined" : <td>{user.isGoing === true ?  "Yes" : "No" }</td>}
-        <td>{user.guests === null || user.guests === undefined ? "NOT SET" : user.guests}</td>
+        {user.isGoing === null || user.isGoing === undefined ? <td className="isGoing-Unknown">Not Determined</td> : <td className={"isGoing-" +  (user.isGoing === true ?  "Yes" : "No") }> {user.isGoing === true ?  "Yes" : "No" }</td>}
+        <td>{user.guests === null || user.guests === undefined ? "Not Set" : user.guests}</td>
         <td>{confirmDelete}</td>
         </tr>
     })
